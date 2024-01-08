7374494b81b284c74e497a4b9e8b5e85c0535158
Bart Piotrowski <b@bpiotrowski.pl>
2024-01-08 19:39:42 +0100

    Revert "metainfo: Handle skips through heuristics early"
    This reverts commit dd8b27f5f524916236ff07c473fee4376f009ebf.

--- flatpak_builder_lint/checks/metainfo.py
+++ flatpak_builder_lint/checks/metainfo.py
@@ -17,23 +17,7 @@ class MetainfoCheck(Check):
         metainfo_exts = [".appdata.xml", ".metainfo.xml"]
         metainfo_path = None
 
-        if appid.endswith(".BaseApp"):
-            return
-
-        if not os.path.exists(appstream_path):
-            self.errors.add("appstream-missing-appinfo-file")
-            return
-
-        if len(appstream.components(appstream_path)) != 1:
-            self.errors.add("appstream-multiple-components")
-            return
-
-        if appstream.component_type(appstream_path) not in (
-            "desktop",
-            "desktop-application",
-            "console-application",
-        ):
-            return
+        is_baseapp = appid.endswith(".BaseApp")
 
         for metainfo_dir in metainfo_dirs:
             for ext in metainfo_exts:
@@ -62,16 +46,18 @@ class MetainfoCheck(Check):
                 if not appstream.check_caption(appstream_path):
                     self.warnings.add("appstream-screenshot-missing-caption")
 
-                appinfo_validation = appstream.validate(metainfo_path)
-                if appinfo_validation["returncode"] != 0:
-                    self.errors.add("appstream-failed-validation")
-                    for err in appinfo_validation["stderr"].split(":", 1)[1:]:
-                        self.appstream.add(err.strip())
-                    for out in appinfo_validation["stdout"].splitlines()[1:]:
-                        self.appstream.add(re.sub("^\u2022", "", out).strip())
+                if not is_baseapp:
+                    appinfo_validation = appstream.validate(metainfo_path)
+                    if appinfo_validation["returncode"] != 0:
+                        self.errors.add("appstream-failed-validation")
+                        for err in appinfo_validation["stderr"].split(":", 1)[1:]:
+                            self.appstream.add(err.strip())
+                        for out in appinfo_validation["stdout"].splitlines()[1:]:
+                            self.appstream.add(re.sub("^\u2022", "", out).strip())
 
         if not os.path.exists(icon_path):
-            self.errors.add("appstream-missing-icon-file")
+            if not is_baseapp:
+                self.errors.add("appstream-missing-icon-file")
 
     def check_build(self, path: str) -> None:
         appid = builddir.infer_appid(path)
@@ -81,10 +67,17 @@ class MetainfoCheck(Check):
         metadata = builddir.get_metadata(path)
         if not metadata:
             return
-        if metadata.get("extension"):
-            return
+        is_extension = metadata.get("extension")
 
         self._validate(f"{path}", appid)
+
+        if is_extension:
+            self.errors.discard("appstream-failed-validation")
+            self.errors.discard("appstream-missing-icon-file")
+
+        appstream_path = f"{path}/files/share/app-info/xmls/{appid}.xml.gz"
+        if os.path.exists(appstream_path) and appstream.is_console(appstream_path):
+            self.errors.discard("appstream-missing-icon-file")
 
     def check_repo(self, path: str) -> None:
         self._populate_ref(path)
@@ -97,5 +90,7 @@ class MetainfoCheck(Check):
             ret = ostree.extract_subpath(path, ref, "/", tmpdir)
             if ret["returncode"] != 0:
                 raise RuntimeError("Failed to extract ostree repo")
-
+            appstream_path = f"{tmpdir}/files/share/app-info/xmls/{appid}.xml.gz"
             self._validate(tmpdir, appid)
+            if os.path.exists(appstream_path) and appstream.is_console(appstream_path):
+                self.errors.discard("appstream-missing-icon-file")
